e8dbcd989db9e7e862724dc2286148bdafec2fea
anthill499 <anthill499@gmail.com>
2021-07-09 08:59:47 -0400

    octopus hw

--- W4D5/octopus_problems.rb/octopus_problems.rb
+++ W4D5/octopus_problems.rb/octopus_problems.rb
@@ -1,4 +1,5 @@
 $fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+$tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
 
 def sluggish_octupus
     arr = []
@@ -18,6 +19,14 @@ def clever_octopus
     end
     winner
 end
-p sluggish_octupus
+# p sluggish_octupus
 # p dominant_octopus
-p clever_octopus
+# p clever_octopus
+
+def slow_dance(str)
+    $tiles_array.each_with_index { |direction, index| return index if str == direction }
+end
+p slow_dance("up")
+p slow_dance("right-down")
+
+

45e8b296390b931598d307f773e29739bbcba713
anthill499 <anthill499@gmail.com>
2021-08-05 22:40:21 -0400

    lol

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -17,13 +17,13 @@ document.addEventListener("DOMContentLoaded", function(){
     //phase 4
 
     ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.arc(75, 175, 50, 0, Math.PI * 2, true); // Outer circle
     ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.arc(75, 175, 35, 0, Math.PI, false);  // Mouth (clockwise)
     ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.arc(60, 165, 5, 0, Math.PI * 2, true);  // Left eye
     ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.arc(90, 165, 5, 0, Math.PI * 2, true);  // Right eye
     ctx.stroke();
 
 });
